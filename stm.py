f6f4144de58adeeea1b87bfaf88c3aea1d2a3d20
W2C9I <dobrbobr599@gmail.com>
2025-09-18 22:23:48 +0700

    Bugfixes
    1. пофиксил неправильный подсчет времени подъема и высоты подъема (t_ymax; Y_max)
    2. добавил проверку, защищающую от создания пустых графиков (например вы задаете угол >= 180 при Y0 = 0)

--- stm.py
+++ stm.py
@@ -20,24 +20,32 @@ speed0y = speed0 * np.sin(anglerad)
 coef = [a, speed0y, Y0]
 roots = np.roots(coef)
 t_max = 0
+t_ymax = 0
 if roots[0] > roots[1]:
-    t_max = round(roots[0])
-else: t_max = round(roots[1])
-X_max = X0 + (t_max * speed0x)
-t_ymax = speed0y/g
-Y_max = Y0 + (speed0y * t_ymax) - ((g * t_ymax**2) / 2)
-print("Максимальная дальность полета: ", round(abs(X_max), 2), 'метров')
-print("Максимальная высота подъема: ", round(Y_max, 2), 'метров')
-print("Время полета: ", round(t_max, 1), 'секунд')
-tpl = Y_max * 1.7
-t_val = np.linspace(0, t_max, 1500)
-X_val = X0 + speed0x * t_val
-Y_val = Y0 + (speed0y * t_val) - ((g * t_val**2) / 2)
-plt.figure(figsize=(100, 300))
-plt.plot(X_val, Y_val)
-plt.xlabel('Ось X')
-plt.ylabel('Ось Y')
-plt.grid(True)
-plt.ylim(top = tpl)
-plt.gca().set_aspect('equal', adjustable='box')
-plt.show()
+    t_max = round(roots[0], 1)
+else: t_max = round(roots[1], 1)
+if t_max <= 0:
+    print("тело сразу же ударяется о землю, смысла нет")
+else:
+    X_max = X0 + (t_max * speed0x)
+    if speed0y > 0:
+        t_ymax = speed0y / g
+    else:
+        t_ymax = 0
+
+    Y_max = Y0 + (speed0y * t_ymax) - ((g * t_ymax ** 2) / 2)
+    print("Максимальная дальность полета: ", round(abs(X_max), 2), 'метров')
+    print("Максимальная высота подъема: ", round(Y_max, 2), 'метров')
+    print("Время полета: ", round(t_max, 1), 'секунд')
+    tpl = Y_max * 1.7
+    t_val = np.linspace(0, t_max, 1500)
+    X_val = X0 + speed0x * t_val
+    Y_val = Y0 + (speed0y * t_val) - ((g * t_val ** 2) / 2)
+    plt.figure(figsize=(100, 300))
+    plt.plot(X_val, Y_val)
+    plt.xlabel('Ось X')
+    plt.ylabel('Ось Y')
+    plt.grid(True)
+    plt.ylim(top=tpl)
+    plt.gca().set_aspect('equal', adjustable='box')
+    plt.show()
